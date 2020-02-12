c54ca3e7a8e973fe3dedcbd8fb7b5a8b859696b9
Leon Vong <Lvong2694@conestogac.on.ca>
2020-02-12 09:55:32 -0500

    Made a batman signal using ASCII and printfs.

---
+++ batman/batman.cpp
@@ -0,0 +1,87 @@
+/*
+*
+* batman.cpp
+* Scavenger Hunt Batman Symbol
+* Leon Vong
+* Jan, 26, 2020
+* This is the main file we use to
+* demonstrate how to produce a bat signal using
+* command prompt as it seems to be SET like.
+* The reason why we didn't use File I/O
+* for or while loops, switch/if statements is 
+* because this ain't being marked and other reasons. 
+* Still I comment the header to keep things standards.
+* The original source of this ASCII image can be found
+* with the link below, I do not take claim for any credit
+* of the original drawing. The usage is non-commercial.
+* http://www.asciify.net/ascii/show/5022
+*
+*/
+
+#include <stdio.h>
+
+int main(int argc, char* argv[])
+{
+	printf("                                                  `                                                `                                                  \n");
+	printf("`:i*************+*****+****************+*********:                                                :*********+****************+*****+**************;`\n");
+	printf("`,*###zz####z##z##z##z##z####zz####z##z##z##z##zi                                                iz##z##z##z##z####zz####z##z##z##z##z####zz##+i:``\n");
+	printf("   `:+#zz#zzzzz#z#zzz#z#zzzzz#zz#zzzzz#n#zzz#z#zz+`                  .        .                  `+zz#z#zzz#n#zzzzz#zz#zzzzz#z#zzz#z#zzzzz#zz##:`\n");
+	printf("      `:+z#zz#zz#z#zzz#z#zz#zz#zz#zzzzz#n#zzz#z#zz+:                  i`       i                  ,#zz#z#zzz#n#zzzzz#zz#zz#zz#z#zzz#z#zz#zz#z#;`\n");
+	printf("       .i#z##zz#z#zz##z#zz##z#zz#z##zz#z##z##z#zz#;                 `i.      `#.                 ,#zz#z##z##z#zz##z#zz#z##zz#z##zz#z#zz##z+*. \n");
+	printf("         ,+zznzznzznzznzznzzzznnznzznzznzznzznzznz+                 `#,      .#`                 +znzznzznzznzznzznznnzzzznzznzznzznzznzz#,\n");
+	printf("          .;+z##z##z##z##z####zz#z##z##z##z##z##z##.                `+;      ;+.                .##zz#z##z##z##z##z#zz####z##z##z##z##z+*.\n");
+	printf("           `;#z#z#zzz#z#zzzzz#zz#zzzzz#z#zzz#z#zz#zi                .#+`    `*#,               `iz#zz#z#zzz#z#zzzzz#zz#zzzzz#z#zzz#z#z#i`\n");
+	printf("             :+#z#zzz#z#zz#zz#zz#zzzzz#n#zzzzz#zz#zz:               `#zii++ii##,               ,zz#zz#zzzzz#n#zzzzz#zz#zz#zz#z#zzz#z##:\n");
+	printf("             `:+z##z##z#zz##z#zz#z##zz#z##z##z#zz##z+,`             ,#zz#zz#zz#,             `.+z##zz#z##z##z#zz##z#zz#z##zz#z##z##z+:`\n");
+	printf("                .zzznzznzznzzzznzzzzznzznzznzznzznzzzz#,             :#znzzzznz#;             .#zzzznzznzznzznzznzzzzznzzzznzznzznzzn. \n");
+	printf("                 ,+#z##z##z####zz#z##z##z##z##z#zz####z#;.           ,+zz####zz#;           .;#z####zz#z##z##z##z##z#zz####z##z##z##;\n");
+	printf("                  ;#zz#z#zzzzz#zz#zzzzz#z#zzz#z#zz#zz#zz##*:,.    `  i#zz#zz#zz#*  `  ``.,;i##zz#zz#zz#z#zzz#z#zzzzz#zz#zzzzz#z#zz#i \n");
+	printf("                   ;zz#n#zzzzz#zz#zzzzz#n#znzzz#zz#zz#zz#zzznz++ii;;*n#zz#zz#zz#n*iiii++znzzz#zz#zz#zz#zzznz#n#zzzzz#zz#zzzzz#n#zz;` \n");
+	printf("                  .+##z#zz##z#zz#z##zz#z##z##z#zz##z#zz#z##zz#z#zz##z#zz#zz#zz#z##zz#z#zz##z#zz#z##zz#z##z##z#zz##z#zz#z##zz#z###, \n");
+	printf("                    ;#znzznzzzzzzzzzznzznzznzznzznzzzznzznzznzznzznzznzznzzzznzznzznzznzznzznzznzzzznzznzznzznzznzzzzzzzzzznzznzzi \n");
+	printf("                    `+#z#zz##z#zz#z##z##z##z##z#zz####zz#z##z##z##z##z#zz####zz#z##z##z##z##z#zz####zz#z##z##z##z##z#zz#z##zz#z#*`\n");
+	printf("                     :zz#zz#zz#zz#zzzzz#z#zzz#z#zz#zz#zz#zzzzz#z#zzz#z#zz#zz#zz#z#zzz#z#zzzzz#zz#zz#zz#z#zzz#z#zzzzz#zz#zz#zz#z#;  \n");
+	printf("                      +n#znzzz#zz#zzznz#n#znzzz#zn#zz#zz#zzznz#n#znzzz#zz#zz#zz#zzznz#n#znzzz#zz#zz#nz#zzznz#n#znzzz#zz#zzznz#n+`\n");
+	printf("                      ;z#zz##z#zz#z##zz#z##z##z#zz##z#zz#z##zz#z##z##z#zz####zz#z##z##z#zz##z#zz#z##zz#z##z##z#zz##z#zz#z##zz#z;\n");
+	printf("                      .nzznzzzzzzzzzznzznzznzznzznzzzznzznzznzznzznzznzznzzzznzznzznzznzznzznzznzzzznzznzznzznzznzzzzzzzzzznzzn.\n");
+	printf("                       +#zz##z#zz#z##z##z##z##z#zz##z#zz#z##z##z##z##z#zz####zz#z##z##z##z##z#zz#z##zz#z##z##z##z##z#zz#z##zz##` \n");
+	printf("                       *#zz#zz#zz#zzzzz#z#zzz#z#zz#zz#zz#zzzzz#z#zzz#z#zz#zz#zz#z#zzz#z#zzzzz#zz#zz#zz#z#zzz#z#zzzzz#zz#zz#zz#+ \n");
+	printf("                       ;#znzzz#zz#zzznz#n#znzzz#znzzzzzz#zzznzznzznzzz#zzzzzzzz#zzznzznzznzzz#zzzzzznz#zzznz#n#znzzz#zz#zzznz#* \n");
+	printf("                       :#zz##z#zz#z##zz#z##z##z#zz##z#zz#z##zz#z##z##z#zz####zz#z##z##z#zz##z#zz#z##zz#z##z##z#zz##z#zz#z##zz#: \n");
+	printf("                       :#znzzzzzzzzzznzznzznzznzznzzzzzzznzznzznzznzznzznzzzznzznzznzznzznzznzzzzzzznzznzznzznzznzzzzzzzzzznz#: \n");
+	printf("                       ,#zz##z#zz#z##zz#z##z##z#zz##z#zz#z##z##z##z##z#zz####zz#z##z##z##z##z#zz#z##zz#z##z##z#zz##z#zz#z##zz#;\n");
+	printf("                       ,#zz#zz#zz#zz#zz#z#zzz#z#zz#zz#zz#z#zzz#z#zzz#z#zz#zz#zz#z#zzz#z#zzz#z#zz#zz#zz#z#zzz#z#zz#zz#zz#zz#zz#:\n");
+	printf("                       :#znzzzzzzzzzznzzn#znzznzznzzzzzzzzzznzznzznzzzzznzzzznzzzzznzznzznzzzzzzzzzznzznzznzznzznzzzzzzzzzznz#:  \n");
+	printf("                       :#zz##z#zz#z##zz#z##z##z#zz####zz#z##z##z##z##z#zz####zz#z##z##z##z##z#zz####zz#z##z##z#zz##z#zz#z##zz#: \n");
+	printf("                       :#znzzzzzzzzzznzznzznzznzznzzzzzzzzzznzznzznzznzzzzzzzzzznzznzznzznzzzzzzzzzznzznzznzznzznzzzzzzzzzznzzi   \n");
+	printf("                       i#zz##z#zz#z##zz#z##z##z#zz##z#zz#z##zz#z#zz##z#zz#zz#zz#z##zz#z#zz##z#zz#z##zz#z##z##z#zz##z#zz#z##zz#*  \n");
+	printf("                       +#zz##########zz#z##zz#z#zz#zz#zz#z#zzz#z#zzz#z#zz#zz#zz#z#zzz#z#zzz#z#zz#zz#zz#z#zz##z#zz##########zz#+  \n");
+	printf("                       :::,,:,......,,,:::;*i++#znzzzzzzzzzznzznzznzznzznzzzznzznzznzznzznzzzzzzzzzznzz#+**i:::,,,......,:,,:;;  \n");
+	printf("                                            .`.,;i+###zz#z##z##z##z##z#zz####zz#z##z##z##z##z#zz###+i;:.`.  \n");
+	printf("                                                  `,;*zzzzzznzznzznzzzzzzzzzzzzzzzznzznzznzzzzzz*i,`\n");
+	printf("                                                    ``.i+###zz#z#zz##z#zz#zz#zz#z##zz#z#zz###+;. ` \n");
+	printf("                                                        `:+#zz#z#zzz#z#zz#zz#zz#z#zzz#z#zz#+:` \n");
+	printf("                                                          `:*zznzznzznzznzzzznzznzznzznzz+:` \n");
+	printf("                                                            .;+z##z##z#zz####zz#z##z##z+;.       \n");
+	printf("                                                              .+zzzzzzzzzzzzzzzzzzzzzz+,\n");
+	printf("                                                               `;#z##z#zz#zz#zz#z##z#i. \n");
+	printf("                                                                `;#z#z#zz#zz#zz#z#z#;` \n");
+	printf("                                                                  :zznzznzzzznzznzz;`\n");
+	printf("                                                                   :#z#zz####zz#z+:\n");
+	printf("                                                                    iz#zzzzzzzz#ni \n");
+	printf("                                                                     *#zz#zz#zz#+ \n");
+	printf("                                                                     .*zz#zz#zz+.\n");
+	printf("                                                                      :#nzzzznz;\n");
+	printf("                                                                      `;z####zi`\n");
+	printf("                                                                       ,zzzzzz,\n");
+	printf("                                                                       `i#zz#*` \n");
+	printf("                                                                        ,#zz#,\n");
+	printf("                                                                         *zz+`\n");
+	printf("                                                                         :##:\n");
+	printf("                                                                         `#z.\n");
+	printf("                                                                          i+\n");
+	printf("                                                                          ::\n");
+	printf("                                                                          `.\n\n\n\n");             
+
+	return 0;
+}
